000c6acbfd599a95416fddf220adcf8d0052b370
Neha Bhat <nehabhat0918@gmail.com>
2025-11-27 14:48:38 +0530

    Login Page

---
+++ login.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+window = Tk()
+window.geometry("1280x700+0+0")
+window.title("Student Management System")
+bg_img = Image.open("loginNew1.png")
+bg_img = bg_img.resize((1280, 700))
+bg_photo = ImageTk.PhotoImage(bg_img)
+
+bg_label = Label(window, image=bg_photo)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+loginFrame = Frame(
+    window,
+    bg="white",
+    width=380,
+    height=500,
+    highlightbackground="#4a90e2",
+    highlightthickness=3,
+    highlightcolor="#4a90e2"
+)
+loginFrame.place(x=850, y=120)
+
+# TITLE
+Label(loginFrame, text="LOGIN HERE",fg= "MistyRose4", font=("Times New Roman", 24, "bold"), bg="white").place(x=80, y=30)
+
+# USERNAME
+Label(loginFrame, text="Username:",fg= "DodgerBlue4", font=("Cascadia code", 14), bg="white").place(x=40, y=120)
+Entry(loginFrame, font=("Arial", 14), width=25).place(x=40, y=150)
+
+# PASSWORD
+Label(loginFrame, text="Password:",fg= "DodgerBlue4", font=("Cascadia code", 14), bg="white").place(x=40, y=220)
+Entry(loginFrame, font=("Arial", 14), width=25, show="*").place(x=40, y=250)
+
+# LOGIN BUTTON
+Button(loginFrame, text="Login", font=("Arial", 16, "bold"), width=12,
+       bg="#4a90e2", fg="white", relief=FLAT).place(x=110, y=330)
+
+window.mainloop()
