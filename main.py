595e27e1a967d79d055b6835461902d9f274ba43
Krishnaveni <slkrishnaveni2307@gmail.com>
2025-11-27 12:25:06 +0530

    New background

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ from tkinter import *
 from PIL import ImageTk
 window=Tk()
 
-window.geometry("1740x700+0+0")
-window.backgroundImage=ImageTk.PhotoImage(file=r"C:\Users\whynew.in\OneDrive\Desktop\python\PES\UNIT-3\image\b2.jpg")
+window.geometry("1280x700+0+0")
+window.backgroundImage=ImageTk.PhotoImage(file="loginNew1.png")
 bglabel=Label(window,image=window.backgroundImage)
 bglabel.place(x=0,y=0)
 
 loginFrame=Frame(window)
-loginFrame.place(x=550,y=150)
-logoImg=ImageTk.PhotoImage(file="newlogo2.jpg")
-logolabel=Label(loginFrame,image=logoImg)
-logolabel.image=logoImg
-logolabel.grid(row=0,column=0)
 window.mainloop()
-#hi
