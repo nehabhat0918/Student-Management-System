8ec2cf7ada55ec000c6cfafb56b5d94aa3a0635b
Neha Bhat <nehabhat0918@gmail.com>
2025-12-17 21:46:31 +0530

    changes

--- sms.py
+++ sms.py
@@ -120,7 +120,6 @@ class StudentDialog(wx.Dialog):
             self.entries["Mobile"].GetValue(),
             self.entries["Email"].GetValue(),
         ]
-<<<<<<< HEAD:new.py
 
 
 # MAIN DASHBOARD WINDOW
@@ -318,5 +317,3 @@ class MyApp(wx.App):
 if __name__ == "__main__":
     app = MyApp(False)
     app.MainLoop()
-=======
->>>>>>> 90d54f6b26894f80a3b226dd2b670a71b259becf:sms.py
